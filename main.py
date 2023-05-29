c0f4b08a89fc46ff47309b6397273a19bb0ef7ee
Leneyxis <78430937+Leneyxis@users.noreply.github.com>
2023-05-29 12:54:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from datetime import *
+from sklearn.linear_model import *
+from sklearn.model_selection import *
 
-data = pd.read_csv("rosamanhattandata.csv")
 
-data.sort_values(["Registration Date"], axis=0, ascending=[True], inplace=False)
-
-# print(data["Registration Date"])
-x = data["Registration Date"]
-x = x.array.shape(-1,1)
-
-x = x.apply(pd.to_datetime)
-
-y = data["Derived Carpet Area Rate"]
-
-# plt.scatter(x,y)
-# plt.show()
-
-model = LinearRegression(fit_intercept=True).fit(x,y)
-
-r_sq = model.score(x,y)
-print(f"coefficient of determination: {r_sq}")
+df = pd.read_csv("rosamanhattandata.csv")
+#cols=list(df.columns.values)
+#print(cols)
+df['Registration Date'] = pd.to_datetime(df['Registration Date'])    
+df['date_delta'] = (df['Registration Date'] - df['Registration Date'].min())
+a=df['Registration Date'].apply(pd.Timestamp.toordinal)
+x=a
+y=df["Derived Carpet Area Rate"]
+lr=LogisticRegression()
+lr.fit(x.values.reshape(1,-1),y)
+ 
